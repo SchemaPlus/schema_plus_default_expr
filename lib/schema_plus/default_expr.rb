f85a65ef63570e3c4849bd1030ff22fdbd52067c
ronen barzel <ronen@barzel.org>
2015-04-01 15:22:49 +0100

    add abstract adapter

--- lib/schema_plus/default_expr.rb
+++ lib/schema_plus/default_expr.rb
@@ -2,6 +2,7 @@ require 'schema_plus/core'
 
 require_relative 'default_expr/version'
 require_relative 'default_expr/middleware'
+require_relative 'default_expr/active_record/connection_adapters/abstract_adapter'
 require_relative 'default_expr/active_record/connection_adapters/column'
 require_relative 'default_expr/active_record/connection_adapters/postgresql_adapter'
 require_relative 'default_expr/active_record/connection_adapters/sqlite3_adapter'
